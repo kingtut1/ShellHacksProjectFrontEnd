60104d0d87fa6ed50547c125d56811a3c75b1f9d
Pharoah Campbell <pharoahcampbell@gmail.com>
2020-09-26 15:39:34 -0400

    Added navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import NavigationBar from './NavigationBar';
+import {Nav, Navbar} from 'react-bootstrap';
+import {Form, FormControl} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <NavigationBar/>
+      <p> Hello </p>
     </div>
   );
 }
