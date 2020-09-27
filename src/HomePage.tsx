ba7b6eb0563ce8cbf4123244c74f3a4c4afd48bb
Arsh Jain <arshjn@gmail.com>
2020-09-27 19:23:26 +0530

    Homepage

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import {Chart} from 'primereact/chart';
+import { Chart } from 'primereact/chart';
+import Card from 'react-bootstrap/Card';
+import { CardDeck } from 'reactstrap';
+
 
 import './HomePage.css';
 
 const chartData = {
-    labels: ['A', 'B', 'C'],
+    labels: ['Entertainment', 'Shopping', 'Travel', 'Food and Drinks'],
     datasets: [
         {
-            data: [300, 50, 100],
+            label: 'This Month',
+            data: [103.5, 105.50, 89.28, 95.50],
             backgroundColor: [
                 "#42A5F5",
                 "#66BB6A",
@@ -23,32 +27,55 @@ const chartData = {
         }
     ]
 };
-class HomePage extends React.Component<{},{}>{
-    
-    render(){
-        return(
-           <div>
-               <div>
-                   <p className = "CenterText">Here is the graphs</p>
-                   <div>
-                        <Chart type="pie" data={chartData}/>
-                   </div>
-               </div>
-               <div className = "InputArea">
-                   <p className = "CenterText">Monthly Input</p>
-                   <Form>
+let basicOptions = {
+    legend: {
+        labels: {
+            fontColor: '#495057'
+        }
+    },
+    scales: {
+        xAxes: [{
+            ticks: {
+                fontColor: '#495057'
+            }
+        }],
+        yAxes: [{
+            ticks: {
+                fontColor: '#495057'
+            }
+        }]
+    }
+};
+class HomePage extends React.Component<{}, {}>{
+
+    render() {
+        return (
+            <div>
+                <CardDeck>
+                    <Card>
+                        <p className="CenterText">This week</p>
+                        <Chart type="bar" data={chartData} options={basicOptions} />
+                    </Card>
+
+                <Card>
+                <div className="InputArea">
+                    <p className="CenterText">Monthly Input</p>
+                    <Form>
                         <Form.Group>
-                            <p className = "CenterText">Spending</p>
+                            <p className="CenterText">Spending</p>
                             <Form.Control placeholder="Spending" />
-                            <p className = "CenterText">Debts</p>
+                            <p className="CenterText">Debts</p>
                             <Form.Control placeholder="Debts" />
-                            <p className = "CenterText">Savings/Investments</p>
+                            <p className="CenterText">Savings/Investments</p>
                             <Form.Control placeholder="Savings/Investments" />
                         </Form.Group>
-                        <Button className="ButtonSpacing"> Submit </Button>   
+                        <Button className="ButtonSpacing"> Submit </Button>
                     </Form>
-               </div>
-           </div>
+                </div>
+                </Card>
+                </CardDeck>
+
+            </div>
         )
     }
 }
